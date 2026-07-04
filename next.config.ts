4ec4ac3abf92de1cdf03bcfb2bb42e6f08efe57e
Pablo Arauz Caballero <128090351+PabloArauzCaballero@users.noreply.github.com>
2026-07-03 21:54:43 -0400

    d

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,4 @@
-import type { NextConfig } from "next";
+﻿import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
@@ -6,13 +6,7 @@ const nextConfig: NextConfig = {
   poweredByHeader: false,
   reactStrictMode: true,
   images: {
-    unoptimized: true,
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "storage.googleapis.com"
-      }
-    ]
+    unoptimized: true
   }
 };
 
